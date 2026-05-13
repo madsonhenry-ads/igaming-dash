927ebae68549350aa3188f5f8460c7e70f9a3039
madsonhenry <madsonhenry.ads@gmail.com>
2026-05-13 22:41:01 +0100

    fix: fix container permissions and use --skip-generate for faster/safer startup

--- dashboard/startup.sh
+++ dashboard/startup.sh
@@ -4,7 +4,7 @@ set -e
 echo "=> Running Prisma migrations..."
 # Define a cache local para evitar erros de permissão
 export PRISMA_GENERATE_SKIP_CACHE=1
-./node_modules/.bin/prisma db push --accept-data-loss
+./node_modules/.bin/prisma db push --accept-data-loss --skip-generate
 ./node_modules/.bin/tsx scripts/seed.ts
 
 echo "=> Starting application..."
